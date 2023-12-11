6e9cdf43040e884459745ca194f2130b9861c49c
your_username <your_email@gmail.com>
2023-12-11 08:38:21 +0100

    ppo

--- test_agents.py
+++ test_agents.py
@@ -4,27 +4,28 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from keras import layers
 
-def get_actor_critic_model(input_shape, actor_out_shape, hidden_size):
-    inputs = layers.Input(shape=input_shape)
-    hidden = layers.Dense(hidden_size, activation='relu')(inputs)
-    actor = layers.Dense(actor_out_shape, activation='softmax', name='actor')(hidden)
-    critic = layers.Dense(1, activation='linear', name='critic')(hidden)
+def get_actor_critic_model(state_shape, n_actions, hidden_size):
+    inputs = layers.Input(shape=(state_shape,))
+    x = layers.Dense(hidden_size, activation='relu')(inputs)
+    x = layers.Dense(hidden_size, activation='relu')(x)
+    actor = layers.Dense(n_actions, activation='softmax', name='actor')(x)
+    critic = layers.Dense(1, activation='linear', name='critic')(x)
 
     return keras.Model(inputs=inputs, outputs=[actor,critic])
 
-env = gym.make("CartPole-v1", render_mode='human') 
+env = gym.make("LunarLander-v2", render_mode='human') 
 # env = gym.make("LunarLander-v2")
 
 states_shape = env.observation_space.shape[0]
 n_actions = env.action_space.n
 
-actor_critic = get_actor_critic_model(input_shape=states_shape,
-                                     actor_out_shape=n_actions,
-                                     hidden_size=32
+actor_critic = get_actor_critic_model(state_shape=states_shape,
+                                      n_actions=n_actions,
+                                      hidden_size=128
 )
-actor_critic.load_weights("checkpoints/ac_cartpole.h5")
+actor_critic.load_weights("checkpoints/ppo_lunarlander_final.h5")
 
-max_test_steps = 200
+max_test_steps = 10000
 step_count = 0
 done = 0
 
@@ -38,5 +39,6 @@ while not done:
     state, reward, done, _, _ = env.step(action)
     step_count += 1
     if step_count > max_test_steps:
+        print('End test steps')
         break
 env.close()
